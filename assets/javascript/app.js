986a5c6e148fb8040320376b189d48d1e40a53ab
Kiara Guzman <kiara.kg11@gmail.com>
2019-07-22 05:10:56 -0400

    added eventPreventDefault

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,41 @@
-// Initial array of movies
 $(document).ready(function() {
 
+    // Initial array of characters
     var topics = ["Jon Snow", "Sansa Stark", "Arya Stark", "Tyrion Lannister", "Hodor", "Daenerys Targaryen", "Cersei Lannister", "Tormund Giantsbane", "Walder Frey", "Drogon"];
 
-    // Create topics array buttons
-    function renderButtons(){
-        $('#buttons-view').empty();
-
-        for (var i = 0; i < topics.length; i++) {
-            // Create all buttons
-            var a = $('<button>');
-            a.addClass('game-of-thrones');
-            a.attr('data-name', topics[i]);
-            a.text(topics[i]);
-            $('#buttons-view').append(a);
-        }
-    }
-
-    renderButtons();
-
-    // On button click
-    $(document).on('click', '.game-of-thrones', function() {
-
-        // new variable will log the text data from each button
-        var gameOfThrones = $(this).html();
-        // console.log(gameOfThrones);
-
+    function displayCharacterInfo() {
+        
+        var gameOfThrones = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gameOfThrones + "&api_key=dc6zaTOxFJmzC&limit=10";
-        // console.log(queryURL);
 
         // Creating an AJAX call for the specific character button being clicked
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).done(function(response) {
+        }).then(function(response) {
 
             var results = response.data;
-            // console.log(results);
-            
-            // Empties the div before adding more gifs
-            $('#character-view').empty();
-            for (var j = 0; j < results.length; j++) {
-                var imageDiv = $('<div>');
-                var imageView = results[j].images.fixed_height.url;
-                var still = results[j].images.fixed_height_still.url;
-                    // console.log(imageView);
 
-            var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
-                gifImage.attr('data-state', 'still');
-                $('#character-view').prepend(gifImage);
-                gifImage.on('click', playGif);
+             // Empties the div before adding more gifs
+             $('#character-view').empty();
+             for (var j = 0; j < results.length; j++) {
+                 var imageDiv = $('<div>');
+                 var imageView = results[j].images.fixed_height.url;
+                 var still = results[j].images.fixed_height_still.url;
+ 
+                var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+                 gifImage.attr('data-state', 'still');
+                 $('#character-view').prepend(gifImage);
+                 gifImage.on('click', playGif);
+ 
+                // Pulling ratings for each character
+                var rating = results[j].rating;
+                var displayRated = $('<p>').text("Rating: " + rating);
+                
+                $('#character-view').prepend(displayRated);
+             }
 
-            // Pulling ratings for each character
-            var rating = results[j].rating;
-                // console.log(rating);
-            var displayRated = $('<p>').text("Rating: " + rating);
-            $('#character-view').prepend(displayRated);
-            }
-        });
-
-            // Function to stop and animate gifs
+             // Function to stop and animate gifs
             function playGif() {
                 var state = $(this).attr('data-state');
                 // console.log(state);
@@ -73,25 +49,42 @@ $(document).ready(function() {
                 }
                 
             } // end of on click function
+         });
+    }
+
+
+    // Function for displaying character data
+    function renderButtons(){
+        // Deletes the characters prior to adding new characters(otherwise you'll have repeat buttons)
+        $('#buttons-view').empty();
+
+        // Loops through the array of characters
+        for (var i = 0; i < topics.length; i++) {
+            // Create all buttons
+            var a = $("<button>");
+            a.addClass("game-of-thrones");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $("#buttons-view").append(a);
+        }
+    }
+
+    // This function handles events where the add character button is clicked
+    $("#add-character").on("click", function(event) {
+        event.preventDefault();
+        // Grabs the input from the textbox
+        var character = $("#character-input").val().trim();
+        // The character from the textbox is then added to our array
+        topics.push(character);
+        // Handles the processing of our character array
+        renderButtons();
             
+    }); 
 
-    }); // end of document on click
+    // Adding click event listeners to all elements with a class of "game-of-thrones"
+     $(document).on("click", ".game-of-thrones", displayCharacterInfo);
 
-
-           //adding new button to array
-           $(document).on('click', '#add-character', function(){
-            if ($('#character-input').val().trim() == ''){
-              alert('Input can not be left blank');
-           }
-           else {
-            var topics = $('#character-input').val().trim();
-            topics.push(character);
-            $('#charcter-input').val('');
-            renderButtons();
-            return false;
-
-            }
-
-    });
+    // Calling the renderButtons function to display the initial buttons
+    renderButtons();
 
 });
